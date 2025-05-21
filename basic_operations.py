390233063f1c671876e1e577cab24657e8a5cc7d
alchemist1111 <emoyoeugine@gmail.com>
2025-05-22 00:01:48 +0300

    added arithmetic operations code

---
+++ basic_operations.py
@@ -0,0 +1,13 @@
+# Variable declaration for number1 and number2
+number1 = 10
+number2 = 5
+
+# Arithmetic operations on the variables, +, - & *
+sum = number1 + number2
+diff = number1 - number2
+product = number1 * number2
+
+# Showing the result of the arithmetic operations on the terminal
+print("The sum of number1 and number2 is:", sum)
+print("The difference of number1 and number2 is:", diff)
+print("The product of number1 and number2 is:", product)
