67a5fed52b68d0d3ac4ffb5ac2aac71e235f55ff
alchemist1111 <emoyoeugine@gmail.com>
2025-05-22 00:34:33 +0300

    added simple interest calculation code

---
+++ simple_interest.py
@@ -0,0 +1,10 @@
+# Variable declaration for the simple interest.
+principal = 1000
+rate = 0.05
+time = 3
+
+# Calculating the interest earned
+interest = principal * rate * time
+
+# Showing the interest in the terminal
+print("The simple interest is:", interest)
