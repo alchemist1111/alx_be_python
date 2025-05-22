c10e6dd5dbc3038e2545a7cc95adac31631eb2c9
alchemist1111 <emoyoeugine@gmail.com>
2025-05-22 12:49:16 +0300

    added area calculation code

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,9 @@
+# Variable declaration for the rectangles, length and width
+length = 10
+width = 5
+
+# Calculating the area
+area = length * width
+
+# Showing the result of the calculation in the terminal
+print("The area of the rectangle is:", area)
