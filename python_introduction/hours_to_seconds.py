c39a82ddd1282d605e6cbb3e30a63de7f63e4841
alchemist1111 <emoyoeugine@gmail.com>
2025-05-22 13:04:44 +0300

    corrected seconds convertion formula

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -2,7 +2,7 @@
 hours = 2
 
 # Hours to seconds conversion
-seconds = 3600 * 2
+seconds = hours * 2
 
 # Showing the seconds calculated in the terminal
 print("2 hour(s) in seconds is:", seconds, "seconds")
