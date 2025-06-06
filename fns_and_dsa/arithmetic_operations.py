9a035855be00d79344ebf0f0e924baad6e77cbf3
alchemist1111 <emoyoeugine@gmail.com>
2025-06-06 20:45:58 +0300

    corrected the function parameters

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,5 @@
 # Function definition to perform operations
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     if operation == "add":
         result = num1 + num2
     elif operation == "subtract":
