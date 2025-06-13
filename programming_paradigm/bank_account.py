2343b926e5eb0502e71980b24102f748ac737bb9
alchemist1111 <emoyoeugine@gmail.com>
2025-06-13 23:40:31 +0300

    corrected bank account display function

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -30,5 +30,5 @@ class BankAccount:
         """
           Print the current account balance.
         """
-        print(f"Current Balance: ${self.__account_balance}")
+        print(f"Current Balance: ${self.__account_balance:.2f}")
                     
