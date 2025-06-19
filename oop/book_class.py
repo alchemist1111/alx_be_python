cb372e592121659f59474f4ba8f0ebff4443d2e7
alchemist1111 <emoyoeugine@gmail.com>
2025-06-19 23:46:08 +0300

    added book class code

---
+++ oop/book_class.py
@@ -0,0 +1,20 @@
+# Book class
+class Book:
+    # Constructor
+    def __init_(self, title: str, author: str, year: int):
+        self.title = title
+        self.author = author
+        self.year = year
+        
+        
+    def __del__(self):
+        """Destructor to print a message when the book object is deleted."""
+        print(f"Deleting {self.title}")
+        
+    def __str__(self):
+        """String representation for the book instance."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+    
+    def __repr__(self):
+        """Official representation for the book instance."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"        
